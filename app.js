16581237b26ac00e0b1c2efc4cc053a94ea0c716
Ricanky <magdaleno.ricardo.j@gmail.com>
2025-01-22 18:20:16 -0700

    actualizar palabra en texto

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let amigos = [];
 function agregarAmigo(){
     let nombre = document.getElementById('amigo').value;
     if(nombre === ''){
-        alert('Por favor, inserte un nombre');
+        alert('Por favor, inserta un nombre');
     }
     else{
         amigos.push(nombre);
